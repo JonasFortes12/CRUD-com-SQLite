c231a4ddd6cc104b2474ece10fe28345e6df2aef
Jonas Fortes <jonascforte@hotmail.com>
2025-04-30 16:58:00 -0300

    feat: finish user crud

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,9 +1,15 @@
 import express from 'express'
-import { getAllUsers } from '../controllers/userController.js'
+import { createUser, getAllUsers, deleteUser, updateUser } from '../controllers/userController.js'
 
 
 const router = express.Router()
 
 router.get('/', getAllUsers)
+
+router.post('/', createUser)
+
+router.put('/:id', updateUser)
+
+router.delete('/:id', deleteUser)
 
 export default router
